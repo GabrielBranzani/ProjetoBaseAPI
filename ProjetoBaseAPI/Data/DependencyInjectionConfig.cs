7d42a165d05317d164c28eaba9e96a6e10401c4f
gabriel.branzani <gabrielguimaraesbranzani@homail.com>
2024-11-27 15:33:27 -0300

    refatorar totalmente

--- ProjetoBaseAPI/Data/DependencyInjectionConfig.cs
+++ ProjetoBaseAPI/Data/DependencyInjectionConfig.cs
@@ -15,21 +15,11 @@ namespace ProjetoBaseAPI.Configuration
 
 			// Repositórios
 			services.AddScoped<IAuthRepository, AuthRepository>();
-			services.AddScoped<IGrupoRepository, GrupoRepository>();
-			services.AddScoped<IModuloRepository, ModuloRepository>();
-			services.AddScoped<IMenuRepository, MenuRepository>();
-			services.AddScoped<IFormularioRepository, FormularioRepository>();
-			services.AddScoped<IPermissaoRepository, PermissaoRepository>();
-			services.AddScoped<IMenuUsuarioRepository, MenuUsuarioRepository>(); // Novo repositório
+
 
 			// Serviços
 			services.AddScoped<AuthService>();
-			services.AddScoped<GrupoService>();
-			services.AddScoped<ModuloService>();
-			services.AddScoped<MenuService>();
-			services.AddScoped<FormularioService>();
-			services.AddScoped<PermissaoService>();
-			services.AddScoped<MenuUsuarioService>();
+			
 		}
 	}
 }
--- ProjetoBaseAPI/Models/GrupoUsuarioModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjetoBaseAPI.Models
-{
-	public class GrupoUsuarioModel
-	{
-		public int codUsuario { get; set; }
-		public int codGrupoUsuario { get; set; }
-		public bool staAtivo { get; set; }
-	}
-}
